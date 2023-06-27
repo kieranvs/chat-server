0abb31709a59bb9ade5f7a0f3a5750cef0e70db7
kieranvs <ksockalingam@hotmail.com>
2023-06-27 11:35:36 +0100

    Remove linux support from the starter code to make it more approachable

--- client.cpp
+++ client.cpp
@@ -1,15 +1,8 @@
-#ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
 
 #include <windows.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
-#else
-#include <unistd.h>
-#include <fcntl.h>
-#include <termios.h>
-#include <sys/ioctl.h>
-#endif
 
 #include <stdio.h>
 #include <cstring>
@@ -61,15 +54,8 @@ void refresh_editline()
 
 int main(int argc, char* argv[])
 {
-    // Parse arguments
-    if (argc != 4) fatal("Usage: ip_address port username\n");
-    char* serverIp = argv[1];
-    char* port = argv[2];
-    char* username = argv[3];
-
     // Put the terminal in 'raw' mode
     {
-#ifdef _WIN32
         HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
         DWORD dwMode = 0;
         GetConsoleMode(hOut, &dwMode);
@@ -79,31 +65,14 @@ int main(int argc, char* argv[])
         dwMode &= ~ENABLE_MOUSE_INPUT;
         dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
         SetConsoleMode(hOut, dwMode);
-#else
-        struct termios old{};
-        if (tcgetattr(0, &old) < 0) fatal("Error in tcsetattr");
-        old.c_lflag &= ~ICANON;
-        old.c_lflag &= ~ECHO;
-        old.c_cc[VMIN] = 1;
-        old.c_cc[VTIME] = 0;
-        if (tcsetattr(0, TCSANOW, &old) < 0) fatal("Error in tcsetattr");
-        fcntl(0, F_SETFL, fcntl(0, F_GETFL) | O_NONBLOCK);
-#endif
     }
 
     // Get the terminal size
     {
-#ifdef _WIN32
         CONSOLE_SCREEN_BUFFER_INFO csbi;
         GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
         term_width = csbi.srWindow.Right - csbi.srWindow.Left + 1;
         term_height = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
-#else
-        winsize w{};
-        ioctl(0, TIOCGWINSZ, &w);
-        term_width = w.ws_col;
-        term_height = w.ws_row;
-#endif
     }
     
     // Set up screen
@@ -119,7 +88,6 @@ int main(int argc, char* argv[])
 
         {
             // STDIN ready
-#ifdef _WIN32
             INPUT_RECORD record;
             DWORD numRead;
             if(!ReadConsoleInput(GetStdHandle(STD_INPUT_HANDLE), &record, 1, &numRead))
@@ -132,11 +100,7 @@ int main(int argc, char* argv[])
 
             char c = record.Event.KeyEvent.uChar.AsciiChar;
             if (c == '\r') c = '\n';
-#else
-            int numRead = read(0, buf, 1);
-            if (numRead <= 0) continue;
-            char c = buf[0];
-#endif
+
             editline[editline_len] = c;
             editline_len += 1;
 
