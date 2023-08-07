7e77ced53f52fb274359fd0ce5ec0ee74de20bbb
Kieran Sockalingam <ksockalingam@hotmail.com>
2023-08-07 19:42:17 +0100

    Add linux support to client v2

--- client-v2.cpp
+++ client-v2.cpp
@@ -1,8 +1,20 @@
+#ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
 
 #include <windows.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#else
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <sys/ioctl.h>
+#include <poll.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#endif
 
 #include <stdio.h>
 #include <cstring>
@@ -62,12 +74,15 @@ int main(int argc, char* argv[])
     char* username = argv[3];
     char* password = argv[4];
 
+#ifdef _WIN32
     // Initialize Winsock
     WSADATA wsaData;
     auto iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
     if (iResult != 0) fatal("WSAStartup failed\n");
+#endif
 
     // Connect to the server
+#ifdef _WIN32
     addrinfo* result = NULL;
 
     iResult = getaddrinfo(serverIp, port, nullptr, &result);
@@ -79,7 +94,20 @@ int main(int argc, char* argv[])
     iResult = connect(clientSd, result->ai_addr, (int)result->ai_addrlen);
     if (iResult == SOCKET_ERROR) fatal("Unable to connect to server\n");
 
+    // unsigned long mode = 0; // 0 blocking, 1 non-blocking
+    // ioctlsocket(clientSd, FIONBIO, &mode);
+
     freeaddrinfo(result);
+#else
+    auto host = gethostbyname(serverIp);
+    sockaddr_in sendSockAddr{};
+    sendSockAddr.sin_family = AF_INET;
+    sendSockAddr.sin_addr.s_addr = inet_addr(inet_ntoa(*(in_addr*)*host->h_addr_list));
+    sendSockAddr.sin_port = htons(atoi(port));
+    int clientSd = socket(AF_INET, SOCK_STREAM, 0);
+    int status = connect(clientSd, (sockaddr*) &sendSockAddr, sizeof(sendSockAddr));
+    if(status < 0) fatal("Error connecting to socket!\n");
+#endif
 
     char buf[1024];
 
@@ -91,9 +119,10 @@ int main(int argc, char* argv[])
         recv(clientSd, buf, 1, 0);
         if (buf[0] != 0) fatal("Login rejected\n");
     }
-
+    
     // Put the terminal in 'raw' mode
     {
+#ifdef _WIN32
         HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
         DWORD dwMode = 0;
         GetConsoleMode(hOut, &dwMode);
@@ -103,14 +132,31 @@ int main(int argc, char* argv[])
         dwMode &= ~ENABLE_MOUSE_INPUT;
         dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
         SetConsoleMode(hOut, dwMode);
+#else
+        struct termios old{};
+        if (tcgetattr(0, &old) < 0) fatal("Error in tcsetattr");
+        old.c_lflag &= ~ICANON;
+        old.c_lflag &= ~ECHO;
+        old.c_cc[VMIN] = 1;
+        old.c_cc[VTIME] = 0;
+        if (tcsetattr(0, TCSANOW, &old) < 0) fatal("Error in tcsetattr");
+        fcntl(0, F_SETFL, fcntl(0, F_GETFL) | O_NONBLOCK);
+#endif
     }
 
     // Get the terminal size
     {
+#ifdef _WIN32
         CONSOLE_SCREEN_BUFFER_INFO csbi;
         GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
         term_width = csbi.srWindow.Right - csbi.srWindow.Left + 1;
         term_height = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+#else
+        winsize w{};
+        ioctl(0, TIOCGWINSZ, &w);
+        term_width = w.ws_col;
+        term_height = w.ws_row;
+#endif
     }
     
     // Set up screen
@@ -118,6 +164,7 @@ int main(int argc, char* argv[])
     refresh_editline();
     fflush(0);
 
+#ifdef _WIN32
     auto socket_event = WSACreateEvent();
     iResult = WSAEventSelect(clientSd, socket_event, FD_READ);
     if (iResult != 0) fatal("Error in WSAEventSelect");
@@ -127,18 +174,33 @@ int main(int argc, char* argv[])
         GetStdHandle(STD_INPUT_HANDLE),
         socket_event
     };
+#else
+    pollfd pfds[2];
+    pfds[0].fd = STDIN_FILENO;
+    pfds[0].events = POLLIN;
+    pfds[1].fd = clientSd;
+    pfds[1].events = POLLIN;
+#endif
 
     std::vector<char> message_buf;
 
     while (true)
     {
         memset(buf, 0, sizeof(buf));
-
+#ifdef _WIN32
         DWORD result = WSAWaitForMultipleEvents(sizeof(eventHandles)/sizeof(eventHandles[0]), &eventHandles[0], FALSE, WSA_INFINITE, FALSE);
+#else
+        int poll_result = poll(pfds, 2, -1); // inf timeout
+#endif
 
+#ifdef _WIN32
         if (result == WSA_WAIT_EVENT_0)
+#else
+        if (pfds[0].revents & POLLIN)
+#endif
         {
             // STDIN ready
+#ifdef _WIN32
             INPUT_RECORD record;
             DWORD numRead;
             if(!ReadConsoleInput(GetStdHandle(STD_INPUT_HANDLE), &record, 1, &numRead))
@@ -151,6 +213,11 @@ int main(int argc, char* argv[])
 
             char c = record.Event.KeyEvent.uChar.AsciiChar;
             if (c == '\r') c = '\n';
+#else
+            int numRead = read(0, buf, 1);
+            if (numRead <= 0) continue;
+            char c = buf[0];
+#endif
 
             if (c == '\n')
             {
@@ -168,7 +235,11 @@ int main(int argc, char* argv[])
             }
         }
 
+#ifdef _WIN32
         if (result == WSA_WAIT_EVENT_0 + 1)
+#else
+        if (pfds[1].revents & POLLIN)
+#endif
         {
             // Socket ready
             int bytes = recv(clientSd, buf, sizeof(buf), 0);
@@ -178,8 +249,9 @@ int main(int argc, char* argv[])
             }
             if (bytes < 0)
             {
-
+#ifdef _WIN32
                 if (WSAGetLastError() == WSAEWOULDBLOCK) continue;
+#endif
                 fatal("Server communication error\n");
             }
 
@@ -214,5 +286,9 @@ int main(int argc, char* argv[])
 
         refresh_editline();
         fflush(0);
+#ifndef _WIN32
+        if (pfds[1].revents & (POLLERR | POLLHUP))
+            fatal("Server disconnected\n");
+#endif
     }
 }
